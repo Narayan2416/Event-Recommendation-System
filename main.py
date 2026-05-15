d1e8491380d3b7e6fc175c4bfd39538a20913082
Narayan2416 <phnarayanamoorthy@gmail.com>
2026-05-15 19:29:51 +0530

    improved readability of the code and maintainability

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask
-from app.routes.services import bp as services_bp
+from app.services.services import bp as services_bp
 from app.routes.auth import bp as auth_bp
+from app.routes.routes import bp as routes_bp
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,6 +12,7 @@ app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
 app.register_blueprint(services_bp)
 app.register_blueprint(auth_bp)
+app.register_blueprint(routes_bp)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
